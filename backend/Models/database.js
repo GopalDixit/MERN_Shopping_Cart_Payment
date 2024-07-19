b9145da92cec38b609d1ca5a0521ca05dd94c811
Gopal Dixit <98686075+GopalDixit@users.noreply.github.com>
2024-07-19 13:01:32 +0530

    database.js

--- backend/Models/database.js
+++ backend/Models/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/Auth-db', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/myDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(()=>{
     console.log('MongoDB connected');
 })
